2653da7d80070f55e8f71750263887e32c3223f0
Sakib62 <sakib62.coder@gmail.com>
2023-06-22 23:28:52 +0600

    UI updated, Google  Sheets connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ---about me section---
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 function openTab(event, tabname) {
@@ -11,6 +12,7 @@ function openTab(event, tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 }
 
+// --responsive sidemenu---
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
@@ -19,3 +21,21 @@ function openmenu() {
 function closemenu() {
     sidemenu.style.right = "-200px";
 }
+
+// ---contact-form-portfolio---
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzhTLO0GoJFo-GrjvH85XtpSD_BDKsAi_cwmQVIZZVeRcQKp1ZNpAnTgQZJxo4sZmSp/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function() {
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
